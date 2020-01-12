8b60ece92d881a7361b26fe8f6d387ae544bc9c2
Isaac Owomugisha <archimedesisaac@gmail.com>
2020-01-12 15:27:34 +0300

    Implement greedy for larger instances and randomly remove 10 slices from the optimal solution and check if there's an improvement. Goes within 4 and 3 of the optimal solution for d and e respectively.

--- src/slice/run_slice.sh
+++ src/slice/run_slice.sh
@@ -3,3 +3,5 @@
 python slice.py ./data/a_example.in > ./output/a_example.out
 python slice.py ./data/b_small.in > ./output/b_small.out
 python slice.py ./data/c_medium.in > ./output/c_medium.out
+python slice.py ./data/d_quite_big.in > ./output/d_quite_big.out
+python slice.py ./data/e_also_big.in > ./output/e_also_big.out
--- src/slice/slice.py
+++ src/slice/slice.py
@@ -1,9 +1,5 @@
-# 1. TODO: Read input file
-# 2. TODO: Prepare output folder and code for writing to output folder
-# 3. TODO: Write code to analyze/evaluate how good the current solution is
-# 4. TODO: For each solution, if it's better than the current solution in the output file or if the output file doesn't
-#  exist, write the solution to the output file.
 import sys
+import random
 
 
 def knapsack(items, capacity):
@@ -26,6 +22,40 @@ def knapsack(items, capacity):
     return dp[capacity], sol
 
 
+def greedy(items, capacity, index_to_exclude):
+    items_value = 0
+    sol = []
+    index = len(items) - 1
+    while index >= 0:
+        if index not in index_to_exclude:
+            items_value += items[index]
+            if items_value >= capacity:
+                items_value -= items[index]
+                break
+            sol.append(index)
+        index -= 1
+    return items_value, sol
+
+
+def randomly_remove(items, capacity):
+    index = min(len(items) - 1, 10**8/len(items))
+    first = greedy(items, capacity, {-1})
+    curr_best_sol = first[1]
+    best_sol_value = first[0]
+    while index >= 0:
+        indices_to_exclude = random.sample(range(len(curr_best_sol)), k=20)
+        indices_to_exclude = [curr_best_sol[i] for i in indices_to_exclude]
+        new_sol = greedy(items, capacity, indices_to_exclude)
+        sol_value = new_sol[0]
+        sol = new_sol[1]
+        if sol_value > best_sol_value:
+            curr_best_sol = sol
+            best_sol_value = sol_value
+        index -= 1
+    sys.stderr.write("sol_val: {}\n".format(best_sol_value))
+    return best_sol_value, curr_best_sol
+
+
 if __name__ == '__main__':
     if len(sys.argv) > 1:
         file_location = sys.argv[1].strip()
@@ -34,6 +64,9 @@ if __name__ == '__main__':
         lines = input_data.split('\n')
         _capacity = int(lines[0].split()[0])
         _items = [int(item) for item in lines[1].split()]
-        result = knapsack(_items, _capacity)
+        if _capacity < 10000:
+            result = knapsack(_items, _capacity)
+        else:
+            result = randomly_remove(_items, _capacity)
         print(len(result[1]))
         print(' '.join(map(str, result[1])))
